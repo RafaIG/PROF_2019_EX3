c2ebc172970ed16a71e8a10f35e1e00a56c92334
RafaIG <rafael.ines.guillen@gmail.com>
2020-01-24 15:52:27 +0100

    En primer lugar modificamos esta clase para inyectar dependencias

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -3,13 +3,27 @@ package es.upm.grise.profundizacion2019.ex3;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Time {
-	
-	public static String getFutureTime(long seconds) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
-		time.plusSeconds(seconds);
-		return formatter.format(time);	
+// En primer lugar modificamos esta clase para inyectar dependencias
+
+public final class Time {
+
+	private LocalDateTime actualTime;
+
+//	public static String getFutureTime(long seconds) {
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); 
+//		LocalDateTime date = (LocalDateTime) formatter.parse("2020/01/01 00:00:00");
+//		LocalDateTime time = LocalDateTime.now();
+//		time.plusSeconds(seconds);
+//		return formatter.format(time);	
+//	}
+
+	public Time(final LocalDateTime time) {
+		this.actualTime = time;
+	}
+
+	public String getFutureTime(final long seconds) {
+		final LocalDateTime futureTime = actualTime.plusSeconds(seconds);
+		return DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(futureTime);
 	}
 
 }
