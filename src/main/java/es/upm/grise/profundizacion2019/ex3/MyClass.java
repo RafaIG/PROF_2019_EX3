84caa65a48e963d73c5b997d3df8960f98060e8e
RafaIG <rafael.ines.guillen@gmail.com>
2020-01-24 15:58:20 +0100

    Se anaden dependencias

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -1,13 +1,18 @@
 package es.upm.grise.profundizacion2019.ex3;
 
-import java.time.LocalDateTime;
-
 // Modificacion para resolver fallos con la clase Time
 
 public final class MyClass {
 	
+	private Time actualTime;
+
+	// Un constructor que inyecta la dependencia
+	public MyClass(final Time time) {
+		this.actualTime = time;
+	}
+	
 	public void nextTime(final long seconds) {
-		final String nextTime = new Time(LocalDateTime.now()).getFutureTime(seconds);
+		final String nextTime = actualTime.getFutureTime(seconds);
 		System.out.println(nextTime);
 	}
 
